619413b1ce0fde0028a5c2ee18b4108944f835f4
Vittoria Vassarotti <112390959+Veetss@users.noreply.github.com>
2023-02-07 12:45:05 +0100

    Update LegacyInterceptor.java

--- src/main/java/co/develhope/interceptorsmiddleware1/interceptors/LegacyInterceptor.java
+++ src/main/java/co/develhope/interceptorsmiddleware1/interceptors/LegacyInterceptor.java
@@ -12,8 +12,18 @@ public class LegacyInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        response.setStatus(HttpStatus.GONE.value());
-        return false;
+        if(request.getRequestURL().toString().contains("/legacy")){
+            response.setStatus(HttpStatus.GONE.value());
+        }
+        return true;
+    }
+    
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
     }
 
 }
